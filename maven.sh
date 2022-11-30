9a9c72ceba039b14801cfe4a3676d7d2913d12c0
sushbsush <118243227+sushbsush@users.noreply.github.com>
2022-11-30 19:24:05 +0530

    Update maven.sh

--- maven.sh
+++ maven.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 #delete privous existing repo folder, so that fresh copy can be clone
-rm -rf build_demo
+rm -rf hello-world-war
 
 #cloning the central repo to local workspace
-git clone https://github.com/madhupk963/build_demo.git
+git clone https://github.com/sushbsush/hello-world-war.git
 
 #get in to the git repo directory
-cd  build_demo
+cd  hello-world-war
  
 #before bulding java and maven need to be pre-installed
 #run the maven package command to build .war file
 mvn package
 
 #copy the .war file to webapps under tomcat server in root directory
-#cp -r  target/build_demo.war  /opt/tomcat10v/webapps/
+#cp -r  target/hello-world-war  /opt/tomcat10v/webapps/
 
 #switch to bin to restart tomcat server
 #cd /opt/tomcat10v/bin
